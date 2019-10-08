340490d2acbd97772435d869d4c01cf26e13d840
Alexandr Abramtchik <alexseveneight@gmail.com>
2019-10-08 20:08:27 +0300

    'processing...'

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
